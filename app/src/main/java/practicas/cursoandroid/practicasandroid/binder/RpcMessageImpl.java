e0e32d7e884704b96663d1b4ea72eeff69ddaba6
carcaret <carcaret@icloud.com>
2016-09-08 09:46:08 +0200

    RpcMessagingService is now asynchronous

--- app/src/main/java/practicas/cursoandroid/practicasandroid/binder/RpcMessageImpl.java
+++ app/src/main/java/practicas/cursoandroid/practicasandroid/binder/RpcMessageImpl.java
@@ -2,6 +2,7 @@ package practicas.cursoandroid.practicasandroid.binder;
 
 import android.content.Context;
 import android.os.Binder;
+import android.os.Looper;
 import android.widget.Toast;
 
 public class RpcMessageImpl extends Binder implements RpcMessage {
@@ -13,14 +14,23 @@ public class RpcMessageImpl extends Binder implements RpcMessage {
     }
 
     @Override
-    public void send(String destination, String message) {
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        Toast.makeText(
-                context, String.format("Mensaje: %s - %s", destination, message), Toast.LENGTH_LONG)
-                .show();
+    public void send(final String destination, final String message) {
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                Looper.prepare();
+                try {
+                    Thread.sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Toast.makeText(
+                        context,
+                        String.format("Mensaje: %s - %s", destination, message), Toast.LENGTH_LONG)
+                        .show();
+                Looper.loop();
+            }
+        };
+        new Thread(runnable).start();
     }
 }
